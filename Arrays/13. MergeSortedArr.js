273485266085a0b7d18d431b2af63de9dcb9f258
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-01-29 09:50:59 +0530

    29 Jan 2025 Daily DSA MergeSortedArray

---
+++ Arrays/13. MergeSortedArr.js
@@ -0,0 +1,22 @@
+//Leetcode: 88: Merge Sorted Array: https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+//inputs: nums1/nums2, m/n
+//objective: merge nums1 and nums2;
+
+//Approach: O(nlogn)
+//Step1: loop through nums1 from 'm'.
+//Step2: Whenever we encounter a '0'.
+//Step3: Replace it with element of nums2.
+//Step4: sort the array.
+
+var merge = function(nums1, m, nums2, n) {
+    let j = 0;
+    for( let i = m; i < nums1.length; i++){
+        if(nums1[i] === 0){
+            nums1[i] = nums2[j];
+            j++;
+        }
+    }
+    nums1.sort((a,b)=> a-b);
+};
