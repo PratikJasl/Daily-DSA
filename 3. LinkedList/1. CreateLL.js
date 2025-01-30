595bbafc6382b9a709b182a04afa20b9b736157c
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-01-30 09:32:40 +0530

    30 Jan 2025 Daily DSA

---
+++ 3. LinkedList/1. CreateLL.js
@@ -0,0 +1,81 @@
+class node{
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null
+        }
+
+        this.tail = this.head;
+        this.length = 1;
+    }    
+
+    append(value){
+        const newNode = new node(value);
+        this.tail.next = newNode;
+        this.tail = newNode
+        this.length ++;
+    } 
+    
+    prepend(value){
+        const newNode = new node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
+
+    print(){
+        const array = [];
+        let current = this.head;
+        while(current != null){
+            array.push(current.value);
+            current = current.next;
+        }
+        console.log(array);
+    }
+
+    insert(index, value){
+        if(index >= this.length){
+            return this.append(value);
+        }
+        if(index === 0){
+            return this.prepend(value);
+        }
+
+        let newNode = new node(value);
+        let pointer = this.head;
+        for(let i = 0; i < index-1; i++){
+           pointer = pointer.next;
+        }
+        newNode.next = pointer.next;
+        pointer.next = newNode;
+        this.length++;
+    }
+
+    delete(index){
+        let pointer = this.head;
+        for(let i = 0; i < index-1; i++){
+            pointer = pointer.next;
+        }
+        let unwantedNode = pointer.next;
+        pointer.next = unwantedNode.next;
+        this.length--;
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 20);
+myLinkedList.insert(0, 20);
+myLinkedList.print();
+myLinkedList.delete(20);
+myLinkedList.print();
+
