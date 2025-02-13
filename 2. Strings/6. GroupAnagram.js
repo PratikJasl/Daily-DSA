332b813c112307101688cb9664b0527fbcb3aa21
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-02-13 09:33:43 +0530

    13 Feb 2025 Daily DSA

--- 2. Strings/6. GroupAnagram.js
+++ 2. Strings/6. GroupAnagram.js
@@ -1,49 +1,12 @@
-
+//Leetcode: 49: Group Anagrams
+//https://leetcode.com/problems/group-anagrams/
 
 //Approach1: 
-
-var groupAnagrams = function(strs) {
-    // let output = [];
-    // let valid = new Set();
-    // //Determines the valid anagrams.
-    // function validAnagram(left, right, strs){
-    //     let temp = []
-    //     while(left >= 0 && right < strs.length){
-    //         if(strs[left].split('').sort().join() === strs[right].split('').sort().join()){
-    //             temp.push(strs[right]);
-    //             valid.add(strs[right]);
-    //             right++;
-    //         }else{
-    //             right++;
-    //         }
-    //     }
-    //     return temp;
-    // }
-    // //Checks if the anagram already exists in the output
-    // function validateExist(longestVal){
-    //     if(output.length < 1) return true
-    //     for(let i = 0; i < longestVal.length; i++){
-    //         if(valid.has(longestVal[i])){
-    //             return false;
-    //         }else{
-    //             return true;
-    //         }
-    //     }
-    // }
-
-    // for(let i = 0; i < strs.length; i++){
-    //     let longestVal = validAnagram(i, i , strs);
-    //     console.log("longestVal:", longestVal);
-    //     console.log("Valid:", valid)
-    //     if(validateExist(longestVal)){
-    //         output.push(longestVal);
-    //         console.log('Output', output);
-    //     }
-    // }
-
-    // return output;
-};  
-
+ //Step1: Loop through the strings.
+ //Step2: Create a sorted string variable.
+ //Step3: if soreted string is not in map, add it to the map with an empty array.
+ //Step4: Get the empty array using sorted string and push the original string inside.
+ //Step5: Create an array of the map values and return it.
 
 function groupAnagrams(strs){
     let map = new Map();
