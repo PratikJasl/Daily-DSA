4fb241a0567f6710d351ced49e888f9b8555605c
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-04-24 10:36:05 +0530

    Basic Math

---
+++ 5. Basic Math/Pallindrome.py
@@ -0,0 +1,19 @@
+#Objective: Check if a number is pallindrome or not.
+
+#Input: 121
+#Output: true
+
+#Concept: We already saw how to reverse a digit, compare the original and reversed digit
+
+def IsPallindrome(N):
+    original = N
+    revNum = 0
+    while(N > 0):
+        lastDigit = N % 10
+        N = int(N / 10)
+        revNum = (revNum * 10) + lastDigit
+    print(revNum == original)
+    return revNum == original
+
+
+IsPallindrome(122)
---
+++ 5. Basic Math/ReverseDigit.py
@@ -0,0 +1,19 @@
+#Objective: Reverse a digit, ig it has trailing digits those will not be included.
+#Input: 1400 
+#Output: 1400--> 0041 --> 41
+
+#Concept: The extraction of digits concept extracts the digit in reverse order. We use it to extract digits 
+# then multiple it with a base of 10 and add the next digits.
+
+
+def ReverseNumber(N):
+    Sum = 0
+    while(N > 0):
+        lastDigit = N % 10 
+        N = int(N / 10)
+        Sum = (Sum * 10) + lastDigit
+    
+    print(Sum)
+    return Sum
+
+ReverseNumber(1400)
