aa99ba381ece7c49b7b0b9813136c74456dd4ee1
PRATIK <exp-jitibj@lato-bicycles.com>
2025-05-23 14:03:08 +0530

    Daily DSA

--- 5. Sorting/QuickSort.js
+++ 5. Sorting/QuickSort.js
@@ -1,1 +1,5 @@
 //@dev: Quick Sort can sort elements in both ascending and descending order with little modifications.
+//Step1: Pick a pivot place it in the correct place.
+//Step2: place all the elements smaller to pivot to left and all the elements greater than pivot to right.
+//Step3: repeat step 1 & 2.
+
