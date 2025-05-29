95d566a1b375b5f6926d08428f7d4b5e791ad50b
PRATIK <exp-jitibj@lato-bicycles.com>
2025-05-29 10:39:20 +0530

    Daily DSA: Array Questions Easy

---
+++ 6. Arrays/5. LeftRotateBy1.js
@@ -0,0 +1,17 @@
+//Q: Given an array of N integrers, left rotate the array by one place.
+
+//Input: [1,2,3,4,5]
+//Output: [2,3,4,5,1]
+
+//Approach: T: O(N) | S: O(1)
+//Step1: Use the splice operator from 1 to end.
+//Step2: We add the spliced part to the front of the array.
+
+function LeftRotateBy1(arr){
+    let newArr = arr.splice(1, arr.length-1);
+    arr.unshift(...newArr);
+    console.log("New Array:", arr);
+    return arr;
+}
+
+LeftRotateBy1([1,2,3,4,5]);
