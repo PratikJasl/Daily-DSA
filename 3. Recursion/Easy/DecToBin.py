786733b4ab8af13b174d07a84eaee003f8dc315e
PRATIK <exp-jitibj@lato-bicycles.com>
2025-07-17 10:00:39 +0530

    Daily DSA topic: Recursion

--- 3. Recursion/Easy/DecToBin.py
+++ 3. Recursion/Easy/DecToBin.py
@@ -1,11 +1,11 @@
 #Q: Convert a given decimal to binary
 #Appraoch: 
 # step1: Module the decimal by 2
-# step2:
+# step2: Add (10 * funCall(d//2))
 
 
 def dec_to_bin(d):
-    if(d== 0):
+    if(d == 0):
         return 0
     else:
         return (d % 2 + 10 * dec_to_bin(d//2))
