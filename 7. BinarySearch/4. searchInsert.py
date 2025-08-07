d1129ac431bd11e3262d126d83333eb184047299
PRATIK <exp-jitibj@lato-bicycles.com>
2025-08-07 09:45:38 +0530

    Daily DSA: BS Problems

--- 7. BinarySearch/4. searchInsert.py
+++ 7. BinarySearch/4. searchInsert.py
@@ -6,4 +6,18 @@
 # Input: nums = [1,3,5,6], target = 5
 # Output: 2
 
-#We use BS algo here.
+def searchInsert(nums, target):
+    n = len(nums)
+    low = 0
+    high = n-1
+    ans = n
+
+    while (low <= high):
+        mid = (low + high) // 2
+
+        if(nums[mid] >= target):
+            ans = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return ans
