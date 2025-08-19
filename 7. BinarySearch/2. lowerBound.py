23f3885903614de9e0440a96729dfa75f9bb8cd2
PRATIK <exp-jitibj@lato-bicycles.com>
2025-08-19 09:50:18 +0530

    daily DSA

--- 7. BinarySearch/2. lowerBound.py
+++ 7. BinarySearch/2. lowerBound.py
@@ -1,13 +1,15 @@
 # Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
-#find the first element which is >=x.
+#find the first element which is >= x.
 # Example 1:
 # Input Format: arr[] = {1,2,2,3}, x = 2
 # Result: 1
 # Explanation: Index 1 is the smallest index such that arr[1] >= x.
 
 #Approach:
-# Step1: implement BS
-# Step2: if we find an element >=x at mid. decrement high to mid-1 and check no other element before satify the condition.
+# Step1: implement BS but do not return at mid.
+# Step2: if we find an element >= x at mid update output and decrement high to mid-1
+# Step3: if element is not >=x increment low to mid+1
+
 def lowerBound(nums, target):
     n = len(nums)
     low = 0
