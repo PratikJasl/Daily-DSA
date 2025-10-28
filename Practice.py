556901f2394f906262c285e59c4423a4bac91b6d
PRATIK <exp-jitibj@lato-bicycles.com>
2025-10-28 10:46:35 +0530

    Daily DSA

--- Practice.py
+++ Practice.py
@@ -1,17 +1,20 @@
-# Selection Sort
-# Select the minimum and move it to the start.
-# 2 Points, 1 minIndex
+# Bubble Sort
+# Swap and move largest to the end.
+# Pointer i, swapped.
 
-def selectionSort(arr):
+def BubbleSort(arr):
     n = len(arr)
-    for i in range (n):
-        minIndex = i
-        for j in range(i+1, n):
-            if(arr[j] < arr[minIndex]):
-                minIndex = j
-        if(minIndex != i):
-            arr[i], arr[minIndex] = arr[minIndex], arr[i]
-    print("Sorted Array:", arr)
+    while True:
+        swapped = False
+        for i in range(n-1):
+            if(arr[i] > arr[i+1]):
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = True
+        if not swapped:
+            break
+        n -= 1
+    print("Sorted Array :", arr)
     return arr
 
-selectionSort([1,5,7,3,2,4,8,6,9])
+BubbleSort([2,5,1,4])
+            
