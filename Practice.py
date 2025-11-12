1f37de9a2423bd059b73386249de7d691224f498
Shivam <Shivam.modi@tataelectronics.co.in>
2025-11-12 07:50:27 +0530

    DailyDSA: Subarrays

--- Practice.py
+++ Practice.py
@@ -1,32 +1,43 @@
-def longestSubarray(nums, k):
-    n = len(nums)
-    sum = nums[0]
-    maxLen = 0
-    left = 0
-    right = 0
-    print("Initial SUM:", sum)
-    print("Array length", n)
-    while(right < n-1):
-        print("Inside While Loop")
-        print("SUM:", sum)
-        if(sum < k):
-            right += 1
-            sum += nums[right]
-        elif(sum > k):
-            left += 1
-            sum -= nums[left]
-        elif(sum == k):
-            maxLen = max(maxLen, (right - left) + 1)
-            print("MaxLen:", maxLen)
-            sum -= nums[left]
-            right += 1
-            left += 1
-            sum += nums[right]
-    print(maxLen)
-    return maxLen
+# def longestSubarray(nums, k):
+#     n = len(nums)
+#     sum = nums[0]
+#     maxLen = 0
+#     left = 0
+#     right = 0
+#     print("Initial SUM:", sum)
+#     print("Array length", n)
+#     while(right < n-1):
+#         print("Inside While Loop")
+#         print("SUM:", sum)
+#         if(sum < k):
+#             right += 1
+#             sum += nums[right]
+#         elif(sum > k):
+#             left += 1
+#             sum -= nums[left]
+#         elif(sum == k):
+#             maxLen = max(maxLen, (right - left) + 1)
+#             print("MaxLen:", maxLen)
+#             sum -= nums[left]
+#             right += 1
+#             left += 1
+#             sum += nums[right]
+#     print(maxLen)
+#     return maxLen
 
 
-arr = [1, -1, 5, -2, 3]
-k=3
+# arr = [1, -1, 5, -2, 3]
+# k=3
 
-longestSubarray(arr, k)
+# longestSubarray(arr, k)
+
+def loop(arr: [int]) -> int:
+    n = len(arr)
+    for i in range(n):
+        print("i->",i)
+        for j in range(i, n):
+            print("j->",j)
+            for z in range(i,j+1):
+                print("z->",z)
+
+loop([1,2,3,4])
