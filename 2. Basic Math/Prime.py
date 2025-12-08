ef333af1e306ac6b8c7d233faa0d31b49d78b3d2
PratikJussal <pratik.jussal@tataelectronics.co.in>
2025-12-08 15:45:53 +0530

    daily DSA

--- 2. Basic Math/Prime.py
+++ 2. Basic Math/Prime.py
@@ -1,6 +1,5 @@
 # A number that has exactly 2 factors 1 and itself is called a Prime.
-
-#Objective: check if a number is prime or not.
+# Objective: check if a number is prime or not.
 # input: 2 
 # Output: True
 
@@ -9,7 +8,6 @@ def CheckPrime(N):
     for i in range (1, N+1):
         if(N % i == 0):
             count += 1
-
     if(count > 2):
         return False
     else:
