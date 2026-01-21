a2e3e44909dd6e0d0f9152d27390c20690af438e
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-01-21 07:39:43 +0530

    TWO Sum-II

--- 4. Arrays & String/5. TwoSum.py
+++ 4. Arrays & String/5. TwoSum.py
@@ -16,6 +16,34 @@ def twoSum(nums: list[int], target: int) -> list[int]:
             if(nums[i] + nums[j] == target):
                 return [i,j]
 
+# Better Solution: Two Pointer
+# Complexity: T:O(nlogn) | S:O(n)
+# We sort the array, so that we do not have to check all possible pairs.
+# But since the indexes are changed we store the indexes in a DS.
+
+def twoSum(self, nums: list[int], target: int) -> list[int]:
+    # 1. Store pairs of (value, index) so we don't lose track
+    # Input: [3, 2, 4] -> [(3,0), (2,1), (4,2)]
+    nums_with_index = []
+    for i, num in enumerate(nums):
+        nums_with_index.append((num, i))
+    # 2. Sort based on the values (O(N log N))
+    # Becomes: [(2,1), (3,0), (4,2)]
+    nums_with_index.sort()
+    left, right = 0, len(nums) - 1
+    
+    while left < right:
+        curr_sum = nums_with_index[left][0] + nums_with_index[right][0]
+        
+        if curr_sum == target:
+            return [nums_with_index[left][1], nums_with_index[right][1]]
+        
+        elif curr_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+
 # Optimal Solution: Hashing
 # Complexity: T:O(n) | S:O(n)
 def twoSum(nums: list[int], target: int) -> list[int]:
