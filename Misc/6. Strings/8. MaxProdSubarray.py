fc5ebf9d6e1467a811fdb0e235e3b20bbd9efe43
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-01-23 12:18:30 +0530

    Daily DSA

--- Misc/6. Strings/8. MaxProdSubarray.py
+++ Misc/6. Strings/8. MaxProdSubarray.py
@@ -22,4 +22,15 @@ def Max_Product_Subarray(nums):
     return maximum
 
 def MaProductSubarray(nums):
-    pass
+    res = nums[0]
+    cur_min, cur_max = 1, 1
+
+    for n in nums:
+        tmp = cur_max * n
+
+        cur_max = max(n * cur_max, n * cur_min, n)
+        cur_min = min(tmp, n * cur_min, n)
+    
+        res = max(res, cur_max)
+    
+    return res
