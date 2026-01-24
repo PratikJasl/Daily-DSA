54e25f7d4d3bacbd006d48f5df650c634107c7c7
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-01-24 13:53:06 +0530

    daily DSA

--- 4. Arrays & String/8. TopKFreqElement.py
+++ 4. Arrays & String/8. TopKFreqElement.py
@@ -5,23 +5,28 @@
 # k = 2
 # output = [1,2]
 
+#Approach: T:O(n) | S: O(n)
+# Step1: Create a frequency map
+# Step2: Use bucket sort to create a list of element with freq.
+# Step3: Return top k results.
+
 def top_k_frequent_element(nums, k):
     count = {}
     freq = []
     res = []
-    for i in range(len(nums)+1):
+    for i in range(len(nums)+1): #Fill freq array with [] in each index
         freq.append([])
 
-    for n in nums:
+    for n in nums: #Create count map.
         if n in count:
             count[n] += 1
         else:
             count[n] = 1
 
-    for value, count in count.items():
+    for value, count in count.items(): #store value in count index
         freq[count].append(value)
     
-    for i in range(len(freq)-1,0,-1):
+    for i in range(len(freq)-1,0,-1): #iterate from end and return res.
         for n in freq[i]:
             res.append(n)
             if(len(res) == k):
