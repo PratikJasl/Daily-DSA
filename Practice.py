ceef0e088c4391f6e73e6821291dac3e53314502
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-02-05 10:14:23 +0530

    Daily DSA

--- Practice.py
+++ Practice.py
@@ -1,44 +1,24 @@
-# 15. 3 SUM
-# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, 
-# and nums[i] + nums[j] + nums[k] == 0.
-# Notice that the solution set must not contain duplicate triplets.
-
+# Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the upper bound of x.
+# find the first element which is >x.
 # Example 1:
-# Input: nums = [-1,0,1,2,-1,-4]
-# Output: [[-1,-1,2],[-1,0,1]]
-# Explanation: 
-# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
-# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
-# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
-# The distinct triplets are [-1,0,1] and [-1,-1,2].
-# Notice that the order of the output and the order of the triplets does not matter.
+# Input Format: arr[] = {1,2,2,3}, x = 2
+# Result: 3
+# Explanation: Index 3 is the smallest index such that arr[3] > x.
 
-def ThreeSum(nums: list[int]): 
+def UpperBound(nums, target):
     n = len(nums)
-    nums.sort()
-    result = []
+    low = 0
+    high = n - 1
+    output = 0
 
-    for i, a in enumerate(nums):
-        if(a > 0):
-            break
+    while(low <= high):
+        mid = (low + high) // 2
+        if(nums[mid] > target):
+            output = mid
+            high = mid - 1
+        else:
+            low = mid + 1
 
-        if(i > 0 and a == nums[i-1]):
-            continue
+    return output
 
-        left = i+1
-        right = n - 1
-
-        while(left < right):
-            sum = a + nums[left] + nums[right]
-
-            if sum < 0:
-                left += 1
-            elif sum > 0:
-                right -= 1
-            else:
-                result.append([a, nums[left], nums[right]])
-                left += 1
-                while(nums[left] == nums[left-1] and left < right):
-                    left += 1
-            
-        return result
+print(UpperBound(nums=[1,2,3,3,4,5], target=6))
