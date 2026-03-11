0293a1a2b843ededbf58a94f54426f4e9244e33c
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-03-11 11:07:40 +0530

    Daily DSA

--- Practice.py
+++ Practice.py
@@ -1,7 +1,35 @@
-nums = [[1,2], [2,1]]
-new = [1,2]
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
 
-if new not in nums:
-    nums.append(new)
+def reorderll(head):
+    slow = head
+    fast = head
 
-print(nums)
+    # Step1: Place slow pointer at mid.
+    while (fast and fast.next.next):
+        slow = slow.next
+    
+    # Step2: Split the list.
+    current = slow.next
+    slow.next = None
+    prev = None
+    forw = None
+
+    #Step3: Reverse the second half.
+    while(current is not None):
+        forw = current.next
+        current.next = prev
+        prev = current
+        current = forw
+
+    #Step4: Merge LL
+    first = head
+    second = prev
+    while second:
+        temp1, temp2 = first.next, second.next
+        first.next, second.next = second, temp1
+        first, second = temp1, temp2
+
+    
