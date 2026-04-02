f427aedf6a19e1f3e7de1de4b520647d8e4dec25
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-04-02 15:23:42 +0530

    Daily DSA

--- 11. Recursion/Easy/1. PrintN.py
+++ 11. Recursion/Easy/1. PrintN.py
@@ -1,11 +1,14 @@
 #Q1: Print numbers from 1 to N without using loops.
-def Print_1_N(n):
-    count = 1
-    if(count == n):
-        return
 
-    count += 1
-    Print_1_N(count)
 
 
 #Q2: Print numbers from N to 1 without using loops.
+def Print_1_N(n):
+    if(n == 1):
+        print(1)
+        return 1
+    print(n)
+    Print_1_N(n-1)
+    
+
+Print_1_N(11)
