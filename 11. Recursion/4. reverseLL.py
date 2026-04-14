f3267aa66949e956eff3a5fda14d6b3ebfe825b2
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-04-14 10:34:04 +0530

    Daily DSA

---
+++ 11. Recursion/4. reverseLL.py
@@ -0,0 +1,14 @@
+# Reverse a Linked List using Recrusion
+
+def reverseLL(head):
+    if(head is None or head.next is None):
+        return head
+    
+    new_head = reverseLL(head.next)
+
+    
+
+    
+
+
+
